65f7ebd65332dcea785eb793b908c486b7035643
Sundar2k4 <csundar2004@gmail.com>
2024-10-01 15:53:55 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Header from './components/Header';
 import Grid from './components/Grid';
 import Sidebar from './components/Sidebar';
 import ArtifactDetail from './components/ArtifactDetail'; // Import the new component
-import Homepage from './components/Mainpage'; // Import the Homepage component
+import Mainpage from './components/Mainpage'; // Import the Mainpage component
 
 function App() {
   const [isSidebarOpen, setSidebarOpen] = useState(false);
@@ -40,7 +40,7 @@ function App() {
           handleLogout={handleLogout} 
         />
         <Routes>
-          <Route path='/' element={<Homepage />} />
+          <Route path='/' element={<Mainpage />} />
           <Route path="/grid" element={<Grid/>}/>
           <Route path="/artifact/:id" element={< ArtifactDetail/>} /> {/* New Route for Detail Page */}
         </Routes>
