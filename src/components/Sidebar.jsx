a033fbe6af9e259cd45e2cdfd252693a7de4867b
Sundar2k4 <csundar2004@gmail.com>
2024-10-19 09:44:10 +0530

    resolved:error

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -17,7 +17,7 @@ const Sidebar = ({ isOpen, toggleSidebar, isLoggedIn, handleLogout }) => {
     <>
       <ul className={`sidebar ${isOpen ? 'open' : ''}`} id="side">
         <li id="x" onClick={toggleSidebar}>
-          <a href="/some-valid-link">
+          <a href="#">
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368">
               <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
             </svg>
