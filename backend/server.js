fe84c58350c3d4a1d8c10ab6aa51184c2745999a
Sundar.c <csundar2004@gmail.com>
2025-04-22 15:47:54 +0530

    change in DB

--- backend/server.js
+++ backend/server.js
@@ -68,5 +68,5 @@ app.post('/api/artifacts', async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
