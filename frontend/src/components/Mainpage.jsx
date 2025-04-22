2827718da66a5a91076e47e8549ff464618e6d64
Sundar.c <csundar2004@gmail.com>
2025-04-22 15:01:05 +0530

    changes i folder

---
+++ frontend/src/components/Mainpage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Mainpage.css"; // Optional: create this CSS file for styling
+import Grid from "./Grid.jsx";
+import Header from "./Header.jsx";
+
+const Mainpage = () => {
+  return (
+    <div className="homepage">
+      <Header />
+      <Grid />
+    </div>
+  );
+};
+
+export default Mainpage;
