ba89e58c1a8d8458243f0a3969033089c88cdfc6
Sundar.c <csundar2004@gmail.com>
2025-04-22 16:38:18 +0530

    change in DB

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,20 @@
-
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Grid from './components/Grid';
-import ArtifactDetail from './components/ArtifactDetail'; // Import the new component
-import Mainpage from './components/Mainpage'; // Import the Mainpage component
-import Formpage from './components/Formpage';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Grid from "./components/Grid";
+import ArtifactDetail from "./components/ArtifactDetail"; // Import the new component
+import Mainpage from "./components/Mainpage"; // Import the Mainpage component
+import Formpage from "./components/Formpage";
 
 function App() {
- 
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path='/ancient-gallery' element={<Mainpage />} />
-          <Route path="/grid" element={<Grid/>}/>
-          <Route path="/artifact/:id" element={<ArtifactDetail/>} /> {/* New Route for Detail Page */}
-          <Route path="/addart" element={<Formpage/>}/>
+          <Route path="/" element={<Mainpage />} />
+          <Route path="/grid" element={<Grid />} />
+          <Route path="/artifact/:id" element={<ArtifactDetail />} />{" "}
+          {/* New Route for Detail Page */}
+          <Route path="/addart" element={<Formpage />} />
         </Routes>
       </div>
     </Router>
